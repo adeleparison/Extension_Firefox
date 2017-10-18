9515423397585823f37f84f7555d4fcd7153c10a
Adèle Parison <AdeleParison@MacBook-Pro-de-Adele.local>
2017-10-18 14:42:48 +0200

    hola

---
+++ bonheur.js
@@ -0,0 +1,17 @@
+var phrase = [
+ "Où est l'éléphant rose ?",
+ "Si vous pouvez le rêvez, vous pouvez le faire !",
+ "J'ai décidé d'être heureux parce que c'est bon pour la santé"
+];
+
+function choix_phrase() {
+ var e = phrase[
+   Math.round(
+     Math.random()*(phrase.length-1)
+   )
+ ];
+ return e;
+}
+
+
+alert(choix_phrase());
